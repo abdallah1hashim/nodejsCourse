543ff3d66e02f5486a01fa00985ec2da62104803
Hashim <104575878+abdallah1hashim@users.noreply.github.com>
2023-12-26 23:37:40 +0200

    add working products and shop parts

--- models/product.js
+++ models/product.js
@@ -1,3 +1,5 @@
+const { get } = require("../routes/admin");
+
 const getDb = require("../util/database").getDb;
 
 class Product {
@@ -16,6 +18,18 @@ class Product {
     } catch (err) {
       console.log(err);
     }
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        return products;
+      })
+      .catch((err) => console.log(err));
   }
 }
 
